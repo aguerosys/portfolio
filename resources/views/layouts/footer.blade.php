8019105ee122c5713b96637e44ae9538ff6a8dee
aguerosys <aguerolinkk@gmail.com>
2021-09-21 12:28:43 -0300

    updates

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,85 @@
+<!-- Contact Section-->
+<section class="page-section bg-info text-white" id="contact">
+    <div class="container">
+        <!-- Contact Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase mb-0 text-white">CONCTACTO</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-database"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Contact Section Form-->
+        <div class="column">
+
+            <h5 class="text-center text-uppercase text-white mb-0">Email:</h5>
+            <p class="text-center text-uppercase text-white mx-2" >aguerolink@gmail.com</p>          
+            <h5 class="text-center text-uppercase text-white mb-0">linkedin:</h5>
+            <p class="text-center text-uppercase text-white mx-2" >linkedin.com/in/aguerodev</p>
+        </div>
+    </div>
+</section>
+
+<!-- Footer-->
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Location-->
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">¿De donde soy?</h4>
+                <p class="lead mb-0">
+                    Rio grande,
+                    <br />
+                    Tierra del fuego 9420
+                </p>
+            </div>
+            <!-- Footer Social Icons-->
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Puedes seguirme en</h4>
+                
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/aguerodev/"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/aguerosys"><i class="fab fa-github"></i></a>
+                
+            </div>
+            <!-- Footer About Text-->
+            <div class="col-lg-4">
+                <h4 class="text-uppercase mb-4">INTERESES</h4>
+                <p class="lead mb-0">
+                    DESARROLLO WEB - DESAROLLO MOVIL - DESARROLLO DE SOFTWARE
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Copyright Section-->
+<div class="copyright py-4 text-center text-white">
+    <div class="container">
+        <small>
+            Copyright &copy; aguerodev.com.ar
+            
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+        </small>
+    </div>
+</div>
+<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
+<div class="scroll-to-top d-lg-none position-fixed">
+    <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
+</div>
+<!-- Portfolio Modals-->
+
+
+<!-- Bootstrap core JS-->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+<!-- Contact form JS-->
+<script src="assets/mail/jqBootstrapValidation.js"></script>
+<script src="assets/mail/contact_me.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
+</html>
